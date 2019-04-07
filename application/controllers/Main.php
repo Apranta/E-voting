b8c0fd11dea6d02e37689fee9f5a437ef6ae4bc4
Criptdestroyer <ahmademir1201@gmail.com>
2019-04-07 23:43:16 +0700

    fix bug

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -64,6 +64,9 @@ class Main extends MY_Controller {
                     echo "<script>salah()</script>";
                     exit;
                 }
+            }else{
+                echo "<script>salah()</script>";
+                exit;
             }
             
         }
