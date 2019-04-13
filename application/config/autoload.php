72949eeefb0f977b8634d994cfe10152e304b21c
Criptdestroyer <ahmademir1201@gmail.com>
2019-04-13 22:08:31 +0700

    add

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('database', 'pagination', 'session', 'email', 'form_validation', 'upload');
+$autoload['libraries'] = array('database', 'pagination', 'email', 'form_validation', 'upload');
 
 /*
 | -------------------------------------------------------------------
