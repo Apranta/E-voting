2cdb48e8d4177b71f9609ea31ac234740666b3b3
Criptdestroyer <ahmademir1201@gmail.com>
2019-04-14 10:46:08 +0700

    add

--- application/views/main.php
+++ application/views/main.php
@@ -16,12 +16,12 @@
                                 </div>
                                 <?php } ?>
                             </div>
-                            <a class="carousel-control-prev btn-danger" href="#carouselExampleControls" role="button" data-slide="prev">
-                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                                <span class="carousel-control-prev-icon btn-danger" aria-hidden="true"></span>
                                 <span class="sr-only">Previous</span>
                             </a>
-                            <a class="carousel-control-next btn-danger" href="#carouselExampleControls" role="button" data-slide="next">
-                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                                <span class="carousel-control-next-icon btn-danger" aria-hidden="true"></span>
                                 <span class="sr-only">Next</span>
                             </a>
                         </div>
@@ -43,11 +43,11 @@
                                 <?php } ?>
                             </div>
                             <a class="carousel-control-prev" href="#carouselExampleControls1" role="button" data-slide="prev">
-                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="carousel-control-prev-icon  btn-danger" aria-hidden="true"></span>
                                 <span class="sr-only">Previous</span>
                             </a>
                             <a class="carousel-control-next" href="#carouselExampleControls1" role="button" data-slide="next">
-                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="carousel-control-next-icon btn-danger" aria-hidden="true"></span>
                                 <span class="sr-only">Next</span>
                             </a>
                         </div>
