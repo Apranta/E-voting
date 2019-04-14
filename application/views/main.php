99ba402cfbf764cb35d5d9b655d7fcf54fb3b95b
Criptdestroyer <ahmademir1201@gmail.com>
2019-04-14 10:44:17 +0700

    add

--- application/views/main.php
+++ application/views/main.php
@@ -16,11 +16,11 @@
                                 </div>
                                 <?php } ?>
                             </div>
-                            <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                            <a class="carousel-control-prev btn-danger" href="#carouselExampleControls" role="button" data-slide="prev">
                                 <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                                 <span class="sr-only">Previous</span>
                             </a>
-                            <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                            <a class="carousel-control-next btn-danger" href="#carouselExampleControls" role="button" data-slide="next">
                                 <span class="carousel-control-next-icon" aria-hidden="true"></span>
                                 <span class="sr-only">Next</span>
                             </a>
